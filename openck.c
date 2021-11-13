2cb215c132a65e770e886ccccda107a5bb8523ce
Soreen Desu <ntiniard@ece.auth.gr>
2021-11-13 15:20:27 +0200

    Initialized Files

---
+++ openck.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include "mmio.h"
+#include "coo2csc.h"
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
---
+++ sequential.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include "mmio.h"
+#include "coo2csc.h"
+
+
+
